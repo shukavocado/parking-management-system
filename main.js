a691d89c2b95575629971b086d28e955d8f75937
Your Name <you@example.com>
2022-05-23 20:50:34 +0800

    inital commit

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ MongoClient.connect(
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
